27575e02577970591a2208c7430871592179378b
Patrik Spathon <patrik@spathon.com>
2015-04-15 16:19:28 +0200

    Option to split browserify files to multiple
    E.g. react components in to separate files with a common files

--- ingredients/browserify.js
+++ ingredients/browserify.js
@@ -1,11 +1,11 @@
-var utilities = require('./commands/Utilities');
+var utilities = require('laravel-elixir/ingredients/commands/Utilities');
 var source = require('vinyl-source-stream');
 var parsePath = require('parse-filepath');
 var browserify = require('browserify');
 var elixir = require('laravel-elixir');
+var factor = require('factor-bundle');
 var babelify = require('babelify');
 var gulp = require('gulp');
-
 
 /**
  * Calculate the correct destination.
@@ -37,12 +37,32 @@ var getDestination = function(output) {
 var buildTask = function(src, output, options) {
     var destination = getDestination(output);
 
+    // log and pause
+    var onError = function(e) {
+      console.error('Browserify failed: '+ e);
+      console.log(e.codeFrame);
+      this.emit('end');
+    };
+
     gulp.task('browserify', function() {
-        return browserify(src, options)
-            .transform(babelify, { stage: 0 })
-            .bundle()
-            .pipe(source(destination.saveFile))
-            .pipe(gulp.dest(destination.saveDir));
+        var b = browserify(src, options);
+        b.transform(babelify, { stage: 0 });
+
+        // Split files in to seperate files with a common
+        if(options.splitFiles) {
+          options.splitFiles = options.splitFiles.map(function(file){
+            return destination.saveDir +'/'+ file;
+          });
+          b.plugin(factor, {
+              // File output order must match entry order
+              output: options.splitFiles
+            });
+        }
+
+        b.bundle()
+          .on('error', onError)
+          .pipe(source(destination.saveFile))
+          .pipe(gulp.dest(destination.saveDir));
     });
 };
 
@@ -62,9 +82,14 @@ elixir.extend('browserify', function(src, output, baseDir, options) {
     var search = '/**/*.+(js|jsx|babel)';
 
     baseDir = baseDir || 'resources/js';
-    src = utilities.buildGulpSrc(src, './' + baseDir, search);
     output = output || this.jsOutput;
     options = options || {};
+
+    // If split files isset set the source as new files or user defined
+    if(options.splitFiles) {
+      options.splitFiles = (options.splitFiles === true) ? src : options.splitFiles;
+    }
+    src = utilities.buildGulpSrc(src, './' + baseDir, search);
 
     utilities.logTask('Running Browserify', src);
 
